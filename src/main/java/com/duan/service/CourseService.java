4cae45a59a834042bf6d0c60ecdb781dc2547c62
duanqi417 <3190323371@qq.com>
2023-05-21 15:43:07 +0800

    fitst commit

---
+++ src/main/java/com/duan/service/CourseService.java
@@ -0,0 +1,15 @@
+package com.duan.service;
+
+import com.duan.pojo.Course;
+
+import java.util.List;
+
+
+public interface CourseService {
+    Boolean insertAutoId(Course course);
+    Boolean deleteById(Integer id);
+    List<Course> selectAll();
+    Course selectById(Integer id);
+    Boolean update(Course course);
+    List<Course> selectByscName(String name);
+}
---
+++ src/main/webapp/static/js/index.js
@@ -0,0 +1,4 @@
+//continue跳转
+document.getElementById("continue").onclick = function (){
+    location.href = "/course/main_page";
+}
